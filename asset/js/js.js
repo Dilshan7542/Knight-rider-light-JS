ad17dbce0d05226528cd24e6efe39e3822ea8811
Dilshan7542 <dilshan.maduranga7542@gmail.com>
2023-06-12 21:16:36 +0530

    initialize

---
+++ asset/js/js.js
@@ -0,0 +1,51 @@
+var count=1;
+var onOf=false;
+function ride(){
+    $(`div:nth-child(${count})`).css("background","red");
+    if(count>1){
+    $(`div:nth-child(${count-1})`).css("background","rgba(238, 4, 4, 0.30)");
+    }
+    if (count < 7) {
+    $(`div:nth-child(${count+1})`).css("background","rgba(238, 4, 4, 0.30)");
+    }
+    if(count<6){
+    $(`div:nth-child(${count+2})`).css("background","rgba(238, 4, 4, 0.0)");
+    }
+    if(count>2){
+        $(`div:nth-child(${count-2})`).css("background","rgba(238, 4, 4, 0.0)");
+    }
+    if(count==7){
+        onOf=true;
+    }else if(count==0){
+        onOf=false;
+    }
+    if(onOf){
+    count--;
+    }else{
+    count++;
+    }
+
+}
+var interval=null;
+var startBtn=$("aside>:first-child");
+var endBtn=$("aside>:last-child");
+let audio = new Audio("asset/audio/KnightRider.mp3");
+
+$(startBtn).click(function (){
+    if(interval==null){
+interval=setInterval(ride,200);
+    audio.play();
+    }
+$(this).css("background-color","blue");
+$(endBtn).css("background","white");
+});
+
+$(endBtn).click(function (){
+    if(interval!==null){
+   clearInterval(interval);
+   interval=null;
+    audio.pause();
+    }
+$(this).css("background-color","blue");
+$(startBtn).css("background","white");
+});
